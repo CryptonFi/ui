597f8dd8e27f381eb6ef0a7fa5eedb0cfb3829da
IlyaTsupryk <ilya.tsupryk@gmail.com>
2024-06-04 23:47:02 +0200

    Remove admin app

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Error from './pages/Error';
-import Admin from './admin/Admin';
+import Admin from './pages/Admin';
 import Orders from './pages/Orders';
 
 export const router = createBrowserRouter(
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 const root = resolve(__dirname, 'src');
-const pubDIr = resolve(__dirname, 'public');
+const pubDir = resolve(__dirname, 'public');
 const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
     root,
     plugins: [react(), nodePolyfills()],
-    publicDir: pubDIr,
+    publicDir: pubDir,
     build: {
         outDir,
         emptyOutDir: true,
