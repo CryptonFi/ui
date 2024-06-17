55dd5712af86e81993a7187767487643175811af
IlyaTsupryk <ilya.tsupryk@gmail.com>
2024-06-17 23:31:20 +0200

    Add a progress execution bar to orders view

--- src/components/Orders/OrderItem.tsx
+++ src/components/Orders/OrderItem.tsx
@@ -5,6 +5,7 @@ import { useTonConnectUI } from '@tonconnect/ui-react';
 import { Address } from '@ton/core';
 import { FaRegTrashAlt } from 'react-icons/fa';
 import { PriceComponent } from '../Price.tsx';
+import Progress from '../ui/Progress.tsx';
 
 interface OrderItemProps extends OrderRes {
     selectOrder: ChangeEventHandler<HTMLInputElement>;
@@ -40,68 +41,73 @@ const OrderItem: FC<OrderItemProps> = (props) => {
     };
 
     return (
-        <a href="#" className="p-4 bg-white border border-gray-200 rounded-lg shadow flex items-center">
-            {props.isSelectable ? (
-                <input
-                    type="checkbox"
-                    id={props.orderId}
-                    name="orders"
-                    className="w-5 h-5 mr-3 sm:mr-3 md:mr-3 lg:mr-3 xl:mr-5 text-blue-600 rounded  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    onChange={props.selectOrder}
-                />
-            ) : (
-                <></>
-            )}
-            <div className="w-28 min-w-24">
-                <span className="text-sm">From:</span>
-                {fromPos ? (
-                    <div>
-                        <div className="text-xl flex items-center">
-                            {fromPos.amount}
-                            <img
-                                src={fromPos.imgUrl}
-                                alt={fromPos.currency}
-                                className="size-5 ml-1 inline rounded-full outline outline-[1px] outline-offset-[-1px] outline-stroke"
-                            />
-                            {fromPos.currency}
-                        </div>
-                        <div className="text-sm text-gray-500">
-                            <PriceComponent tokenSymbol={fromPos.currency} amount={fromPos.amount} />
-                        </div>
-                    </div>
+        <a href="#" className="py-3 px-4 bg-white border border-gray-200 rounded-lg shadow flex flex-col">
+            <div className="w-full flex items-center">
+                {props.isSelectable ? (
+                    <input
+                        type="checkbox"
+                        id={props.orderId}
+                        name="orders"
+                        className="w-5 h-5 mr-3 sm:mr-3 md:mr-3 lg:mr-3 xl:mr-5 text-blue-600 rounded  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        onChange={props.selectOrder}
+                    />
                 ) : (
-                    <div></div>
+                    <></>
                 )}
-            </div>
-            <FaArrowRightLong className="ml-2 mr-6 xl:mr-8" />
-            <div className="w-28 min-w-24">
-                <span className="text-sm">To:</span>
-                {toPos ? (
-                    <div>
-                        <div className="text-xl flex items-center">
-                            {toPos.amount}
-                            <img
-                                src={toPos.imgUrl}
-                                alt={toPos.currency}
-                                className="size-5 ml-1.5 inline rounded-full outline outline-[1px] outline-offset-[-1px] outline-stroke"
-                            />
-                            {toPos.currency}
+                <div className="w-28 min-w-24">
+                    <span className="text-sm">From:</span>
+                    {fromPos ? (
+                        <div>
+                            <div className="text-xl flex items-center">
+                                {fromPos.amount}
+                                <img
+                                    src={fromPos.imgUrl}
+                                    alt={fromPos.currency}
+                                    className="size-5 ml-1 inline rounded-full outline outline-[1px] outline-offset-[-1px] outline-stroke"
+                                />
+                                {fromPos.currency}
+                            </div>
+                            <div className="text-sm text-gray-500">
+                                <PriceComponent tokenSymbol={fromPos.currency} amount={fromPos.amount} />
+                            </div>
                         </div>
-                        <div className="text-sm text-gray-500">
-                            <PriceComponent tokenSymbol={toPos.currency} amount={toPos.amount} />
-                        </div>
-                    </div>
-                ) : (
-                    <div></div>
-                )}
-            </div>
-            {props.isCloseable ? (
-                <div className="ml-auto hover:bg-gray-300">
-                    <FaRegTrashAlt onClick={CloseOrderUI} fontSize="1.3em" />
+                    ) : (
+                        <div></div>
+                    )}
                 </div>
-            ) : (
-                <></>
-            )}
+                <FaArrowRightLong className="ml-2 mr-6 xl:mr-8" />
+                <div className="w-28 min-w-24">
+                    <span className="text-sm">To:</span>
+                    {toPos ? (
+                        <div>
+                            <div className="text-xl flex items-center">
+                                {toPos.amount}
+                                <img
+                                    src={toPos.imgUrl}
+                                    alt={toPos.currency}
+                                    className="size-5 ml-1.5 inline rounded-full outline outline-[1px] outline-offset-[-1px] outline-stroke"
+                                />
+                                {toPos.currency}
+                            </div>
+                            <div className="text-sm text-gray-500">
+                                <PriceComponent tokenSymbol={toPos.currency} amount={toPos.amount} />
+                            </div>
+                        </div>
+                    ) : (
+                        <div></div>
+                    )}
+                </div>
+                {props.isCloseable ? (
+                    <div className="ml-auto hover:bg-gray-300">
+                        <FaRegTrashAlt onClick={CloseOrderUI} fontSize="1.3em" />
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </div>
+            <div className="">
+                <Progress procent={0} />
+            </div>
         </a>
     );
 };
---
+++ src/components/ui/Progress.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+interface ProgressProps {
+    procent: number;
+    style?: string;
+}
+
+const Progress: FC<ProgressProps> = ({ procent, style }) => {
+    return (
+        <div className={`flex flex-row items-center mt-2 ${style}`}>
+            <div className="w-full bg-gray-200 rounded-full h-2 dark:bg-gray-700">
+                <div className="bg-blue-400 h-1.5 rounded-full" style={{ width: `${procent}%` }}></div>
+            </div>
+            <span className="ml-2 text-sm font-medium text-gray-700 dark:text-white">{procent}%</span>
+        </div>
+    );
+};
+
+export default Progress;
